af6bee0508388cc58452aa9f9e79b29d2cf510e0
justice <1393080039@qq.com>
2024-08-31 16:12:06 +0800

    优化

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -1,10 +1,13 @@
 // @ts-ignore
 import { defineClientConfig } from '@vuepress/client'
+// @ts-ignore
 import Home from './Layouts/Home.vue'
+// @ts-ignore
 import Snake from './Layouts/Snake.vue'
 
 // @ts-ignore
 import { defineGiscusConfig } from "vuepress-plugin-comment2/client";
+// @ts-ignore
 import Layout from "./Layouts/Layout.vue";
 
 defineGiscusConfig({
